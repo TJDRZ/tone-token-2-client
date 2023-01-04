9230e0887e404eb5ea3c098fb73f42cac8de1005
TJ <tjdrzewiecki@gmail.com>
2023-01-04 18:26:32 -0500

    Hashrouter test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Menu from "./components/Menu";
 import Login from "./components/Login";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="App">
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/menu" element={<Menu />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
